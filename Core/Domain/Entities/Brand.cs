8624260d8057d17a5c20c6557b93f5f44e1318d6
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-03 18:15:00 +0100

    Entities added

---
+++ Core/Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Brand : BaseEntity
+    {
+        public Brand()
+        {
+            
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/Domain/Entities/Detail.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Detail : BaseEntity
+    {
+        public Detail()
+        {
+            
+        }
+
+        public Detail(string title, string description, int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
