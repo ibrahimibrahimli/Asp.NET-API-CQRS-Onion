66f71edc0fb344574672647781bd5dd347525e94
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-04 21:29:10 +0100

    DbContext Added

---
+++ Infrastructure/Persistance/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using System.Reflection;
+
+    namespace Persistance.Context
+    {
+        public class ApplicationDbContext : DbContext
+        {
+
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Brand> Brands { get; set; }
+            public DbSet<Detail> Details { get; set; }
+            public DbSet<Product> Products { get; set; }    
+            public DbSet<Category> Categories { get; set; }
+
+            override protected void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            }
+        }
+    }
---
+++ Infrastructure/Persistance/Registration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Context;
+
+namespace Persistance
+{
+    public static class Registration
+    {
+        public static void AddPersistance (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
