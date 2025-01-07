3cdd68ec3b45d1e81218a37fd6bcc3f9514c251d
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-07 09:39:01 +0100

    AutoMapper Updated

--- Core/Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
+++ Core/Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -1,33 +1,34 @@
 ﻿using Application.UoW;
 using MediatR;
 using Domain.Entities;
+using Application.Abstracts.AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Application.DTOs;
 
 namespace Application.Features.Products.Queries.GetAllProducts
 {
     public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
     {
         private readonly IUnitOfWork? _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public GetAllProductsQueryHandler(IUnitOfWork? unitOfWork)
+        public GetAllProductsQueryHandler(IUnitOfWork? unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
         {
-            List<GetAllProductsQueryResponse> response = new();
-            var products = await _unitOfWork.GetReadRepository<Product>().GetAllAsync();
-            foreach (var product in products) 
+            var products = await _unitOfWork.GetReadRepository<Product>().GetAllAsync(include: x => x.Include(b => b.Brand));
+            var brand = _mapper.Map<BrandDto, Brand>(new Brand());  
+
+            var map = _mapper.Map<GetAllProductsQueryResponse, Product>(products);
+            foreach (var item in map)
             {
-                response.Add(new GetAllProductsQueryResponse()
-                    {
-                        Title = product.Title,
-                        Description = product.Description,
-                        Discount = product.Discount,    
-                        Price = product.Price - (product.Price * product.Discount / 100),
-                    });
+                item.Price -= ((item.Price * item.Discount) / 100);
             }
-            return response;
+            return map;
         }
     }
 }
---
+++ Mapper/Registration.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstracts.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
