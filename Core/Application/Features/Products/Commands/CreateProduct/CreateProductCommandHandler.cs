f9736da888a3248f533c8e9e82f8ca72338f3458
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-07 10:54:33 +0100

    Commands Added and Entities updated

---
+++ Core/Application/Features/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Abstracts.AutoMapper;
+using Application.UoW;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Products.Commands.CreateProduct
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateProductCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            Product product = new(request.Title, request.Description, request.Price, request.BrandId,request.Discount);
+
+            await _unitOfWork.GetWriteRepository<Product>().AddAsync(product);
+            if(await _unitOfWork.SaveAsync() > 0)
+            {
+                foreach(var categoryId in request.CategoryIds)
+                    await _unitOfWork.GetWriteRepository<ProductCategory>().AddAsync(new()
+                    {
+                        ProductId = product.Id,
+                        CategoryId = categoryId, 
+                    });
+
+                    await _unitOfWork.SaveAsync();
+                
+            }
+        }
+    }
+}
---
+++ Infrastructure/Persistance/Configurations/ProductCategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistance.Configurations
+{
+    public class ProductCategoryConfiguration : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> builder)
+        {
+            builder.ToTable("ProductCategories");
+
+            builder.HasKey(x => new { x.ProductId, x.CategoryId });
+
+            builder.HasOne(p => p.Product)
+                .WithMany(pc => pc.ProductCategories)
+                .HasForeignKey(p => p.ProductId)
+                .OnDelete(deleteBehavior: DeleteBehavior.Cascade);
+
+            builder.HasOne(c => c.Category)
+                .WithMany(pc => pc.ProductCategories)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(deleteBehavior: DeleteBehavior.Cascade);
+        }
+    }
+}
