92b67196293669a1c29ba591e37f2345c2acdbcc
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-08 20:49:26 +0100

    ProductValidation created

---
+++ Core/Domain/Validations/ProductValidation.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validations
+{
+    public class ProductValidation : AbstractValidator<Product>
+    {
+        public ProductValidation()
+        {
+            
+        }
+    }
+}
