b44c2e2a09adbbf72d7628d2d507f5c5038a6f00
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-10 23:03:45 +0100

    Updated

--- Infrastructure/Persistance/Context/ApplicationDbContext.cs
+++ Infrastructure/Persistance/Context/ApplicationDbContext.cs
@@ -1,28 +1,28 @@
-﻿    using Domain.Entities;
+﻿using Domain.Entities;
 using Domain.Entities.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-    using System.Reflection;
+using System.Reflection;
 
-    namespace Persistance.Context
+namespace Persistance.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
     {
-        public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+        }
 
-            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-            {
-            }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Detail> Details { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
 
-            public DbSet<Brand> Brands { get; set; }
-            public DbSet<Detail> Details { get; set; }
-            public DbSet<Product> Products { get; set; }    
-            public DbSet<Category> Categories { get; set; }
-            public DbSet<ProductCategory> ProductCategories { get; set; }
-
-            override protected void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                base.OnModelCreating(modelBuilder);
-                modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            }
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
+}
