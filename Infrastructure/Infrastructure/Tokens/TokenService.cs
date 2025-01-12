340339db1ca1370b471ab2896d43f482e687fa85
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-12 11:27:26 +0100

    TokenService Updated, Program.cs updated

--- Infrastructure/Infrastructure/Tokens/TokenService.cs
+++ Infrastructure/Infrastructure/Tokens/TokenService.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Infrastructure.Tokens
@@ -47,12 +48,30 @@ namespace Infrastructure.Tokens
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[64];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
-        public ClaimsPrincipal? GetPrincipialFromExpiredToken()
+        public ClaimsPrincipal? GetPrincipialFromExpiredToken( string? token)
         {
-            throw new NotImplementedException();
+            TokenValidationParameters validationParameters = new()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenSettings.Secret)),
+                ValidateLifetime = false,
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(token, validationParameters, out SecurityToken securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Token not find!");
+            
+            return principal;
         }
     }
 }
--- Presentation/API/Program.cs
+++ Presentation/API/Program.cs
@@ -2,6 +2,7 @@ using Application;
 using Application.Exceptions;
 using Infrastructure;
 using Mapper;
+using Microsoft.OpenApi.Models;
 using Persistance;
 
 namespace API
@@ -28,6 +29,34 @@ namespace API
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddApplication();
             builder.Services.AddCustomMapper();
+
+            builder.Services.AddSwaggerGen(g =>
+            {
+                g.SwaggerDoc("v1", new OpenApiInfo { Title = "APILearn", Version = "v1", Description = "APILearn swagger client" });
+                g.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "After write 'Bearer' add space and write token \n for excample : 'Bearer eyHJDjJXisd%$6(DjNjnkdj' "
+                });
+                g.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
 
             var app = builder.Build();
 
