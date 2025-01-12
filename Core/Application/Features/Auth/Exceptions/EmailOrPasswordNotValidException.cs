9215d50f3936aa3b009a50855bc4ce6a97527740
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-12 17:01:28 +0100

    LoginController, LoginCommands Added

---
+++ Core/Application/Features/Auth/Exceptions/EmailOrPasswordNotValidException.cs
@@ -0,0 +1,9 @@
+﻿using Application.Bases;
+
+namespace Application.Features.Auth.Exceptions
+{
+    public class EmailOrPasswordNotValidException : BaseException
+    {
+        public EmailOrPasswordNotValidException() : base(BaseUiMessages.EXCEPTION_EMAIL_OR_PASSWORD_NOT_VALID) { }
+    }
+}
--- Core/Application/Features/Auth/Rules/AuthRules.cs
+++ Core/Application/Features/Auth/Rules/AuthRules.cs
@@ -11,5 +11,11 @@ namespace Application.Features.Auth.Rules
             if (user is not null) throw new UserAlreadyExistException();
             return Task.CompletedTask;
         }
+
+        public Task EmailOrPasswordNotValid(User? user, bool checkPassword)
+        {
+            if (user is null || checkPassword) throw new EmailOrPasswordNotValidException();
+            return Task.CompletedTask;
+        }
     }
 }
