37d59f411a97c3c00f3449d043bb270d203d2bf9
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-14 19:51:51 +0100

    RedisService Added

---
+++ Core/Application/Behaviors/RedisCacheBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Application.Abstracts.RedisCache;
+using MediatR;
+
+namespace Application.Behaviors
+{
+    public class RedisCacheBehavior<TRequest, Tresponse> : IPipelineBehavior<TRequest, Tresponse>
+    {
+        private readonly IRedisCacheService _redisCacheService;
+
+        public RedisCacheBehavior(IRedisCacheService redisCacheService)
+        {
+            _redisCacheService = redisCacheService;
+        }
+
+        public async Task<Tresponse> Handle(TRequest request, RequestHandlerDelegate<Tresponse> next, CancellationToken cancellationToken)
+        {
+            if(request is ICacheableQuery query)
+            {
+                var cacheKey = query.CacheKey;
+                var cacheTime = query.CacheTime;
+
+                var cachedData = await _redisCacheService.GetAsync<Tresponse>(cacheKey);
+
+                if(cachedData is not null)
+                    return cachedData;
+
+                var response = await next();
+
+                if(response is not null)
+                    await _redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime  ));
+
+                return response;
+            }
+            return await next(); 
+        }
+    }
+}
---
+++ Infrastructure/Infrastructure/RedisCache/RedisCacheSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.RedisCache
+{
+    public class RedisCacheSettings
+    {
+        public string ConnectionString { get; set; }
+        public string InstanceNumber { get; set; }
+    }
+}
