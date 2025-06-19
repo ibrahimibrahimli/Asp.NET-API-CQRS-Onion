e9dd23a5ee7d4177fb1a3d07fd9a001b2f736930
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-06-19 21:52:33 +0200

    Add delete city functionality to CityController
    - Introduced `DeleteCity` method with authorization for Admins.
    - Updated `GetAllCities` method for consistent formatting.
    - Added using directive for the `Delete` command.

--- Presentation/API/Controller/CityController.cs
+++ Presentation/API/Controller/CityController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Cities.Commands.Create;
+using Application.Features.Cities.Commands.Delete;
 using Application.Features.Cities.Dtos;
 using Application.Features.Cities.Queries;
 using MediatR;
@@ -31,12 +32,24 @@ namespace FitCircleAPI.Controllers
 
         public async Task<IActionResult> GetAllCities()
         {
-            var cities = _mediatr.Send(new GetAllCitiesQueryRequest()); 
-            if(cities != null)
+            var cities = _mediatr.Send(new GetAllCitiesQueryRequest());
+            if (cities != null)
             {
                 return Ok(cities);
             }
             return BadRequest();
+        }
+
+        [HttpDelete("{Id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteCity(DeleteCityCommandRequest request)
+        {
+            var result = await _mediatr.Send(request);
+
+            if (!result)
+                return NotFound(new { Message = "City not found" });
+
+            return NoContent();
         }
     }
 }
