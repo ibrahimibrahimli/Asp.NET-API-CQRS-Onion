64fa5fe40642c933a2ac3397cee31dc6ca06f406
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-06-20 21:16:38 +0200

    Add CurrentUserService and PasetoService implementations
    This commit introduces two new service classes:
    - `CurrentUserService` implements `ICurrentUserService` to provide methods for retrieving current user information, including user ID, username, role, and tokens, using `IHttpContextAccessor`.
    - `PasetoService` implements `IPasetoService` for generating and validating PASETO tokens, handling user claims and token validation parameters.

---
+++ Infrastructure/Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,44 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Infrustructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private HttpContext httpContext => _httpContextAccessor?.HttpContext;
+        private IHeaderDictionary Headers => httpContext?.Request?.Headers;
+
+        public string GetCurrentUserId() => httpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        public string GetCurrentUsername() =>
+              httpContext?.User.FindFirst(ClaimTypes.Name)?.Value;
+
+        public string GetCurrentUserRole() =>
+        httpContext?.User.FindFirst(ClaimTypes.Role)?.Value;
+
+        public string GetCurrentUserFullName() =>
+            httpContext?.User.FindFirst(ClaimTypes.GivenName)?.Value;
+        public string GetUserAgent() =>
+            Headers?["User-Agent"].ToString();
+
+        public string GetFingerprint() =>
+            Headers?["X-Fingerprint"].ToString();
+
+        public string GetRemoteIpAddress() =>
+            Headers?["X-Real-IP"].FirstOrDefault()?? httpContext?.Connection?.RemoteIpAddress?.ToString();
+
+        public string GetRefreshToken() =>
+            httpContext.Request.Cookies["refresh_token"];
+
+        public string GetAccessToken() =>
+            httpContext.Request.Cookies["access_token"];
+
+    }
+}
