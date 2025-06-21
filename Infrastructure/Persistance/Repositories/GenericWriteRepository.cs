c7b2effd6304bb1f781ba9c75a24271fd8bf9cb1
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-06-21 21:18:42 +0200

    Add generic repository classes for data access
    Introduces `GenericReadRepository<T>` and `GenericWriteRepository<T>`
    to implement `IReadRepository<T>` and `IWriteRepository<T>`.
    These classes facilitate data access and manipulation for
    entities inheriting from `BaseAuditableEntity`, providing
    methods for retrieving, adding, updating, and removing
    entities using `FitCircleDbContext` with support for
    asynchronous operations.

---
+++ Infrastructure/Persistance/Repositories/GenericWriteRepository.cs
@@ -0,0 +1,47 @@
+﻿using Application.Abstracts.Repositories;
+using Domain.Common;
+using Persistance.Context;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class GenericWriteRepository<T> : IWriteRepository<T> where T : BaseAuditableEntity
+    {
+        readonly FitCircleDbContext _context;
+
+        public GenericWriteRepository(FitCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<T>().AddAsync(entity, cancellationToken);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<T>().AddRangeAsync(entities, cancellationToken);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
