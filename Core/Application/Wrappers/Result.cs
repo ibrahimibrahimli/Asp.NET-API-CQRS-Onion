3b2b894a3d4a6a79f9d2bf440a78c04a72314fa4
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-06-22 22:18:52 +0200

    Add Result<T> class for operation outcomes
    Introduces a new `Result<T>` class in the `Application.Wrappers` namespace to encapsulate the success or failure of operations. The class includes properties for success status, message, data, and errors, along with static methods for creating successful and failed results.

---
+++ Core/Application/Wrappers/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.Wrappers
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+        public T Data { get; private set; }
+        public List<string> Errors { get; private set; }
+
+        private Result(bool isSuccess, T data, string message, List<string> errors)
+        {
+            IsSuccess = isSuccess;
+            Data = data;
+            Message = message;
+            Errors = errors ?? [];
+        }
+
+        public static Result<T> Success(T data, string message = null) 
+            => new Result<T>(true, data, message ?? "Operation completed succesfully", null);
+
+        public static Result<T> Failure(string message, List<string> errors = null)
+            => new Result<T>(false, default, message ?? "Operation failed", errors);
+    }
+}
