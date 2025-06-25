99114b521befb2ee3a3fae6e60859960ab407036
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-06-25 21:55:21 +0200

    Add CityDto class to Application.DTOs namespace
    Introduced a new `CityDto` class with properties for
    `Id` (Guid), `Name` (string), and `PostalCode` (string).

---
+++ Core/Application/DTOs/CityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs
+{
+    public class CityDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
