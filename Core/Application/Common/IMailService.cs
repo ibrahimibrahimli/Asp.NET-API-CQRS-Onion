e9fce102d29602d117dd8dca6f991023fb3770bb
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-07-24 23:41:42 +0200

    MailService created an implemented IMailService

---
+++ Core/Application/Common/IMailService.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface IMailService
+    {
+        Task SendAsync(string to, string subject, string htmlBody);
+        Task SendAsync(string to, string subject, string htmlBody, string? from, string? displayName);
+        Task SendWithAttachmentAsync(string to, string subject, string htmlBody, List<(byte[] Content, string FileName, string MimeType)> attachments);
+    }
+}
--- Infrastructure/Infrastructure/Services/MailServices/MailService.cs
+++ Infrastructure/Infrastructure/Services/MailServices/MailService.cs
@@ -1,49 +1,94 @@
-﻿using Application.Abstracts.Services;
-using Microsoft.Extensions.Configuration;
+﻿using Application.Common.Interfaces;
+using Domain.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
-
-namespace Infrastructure.Services.MailServices
+namespace Infrustructure.Services
 {
     public class MailService : IMailService
     {
-        readonly IConfiguration _configuration;
-
-        public MailService(IConfiguration configuration)
+        readonly ILogger<MailService> _logger;
+        readonly SmtpSettings _settings;
+        public MailService(ILogger<MailService> logger, IOptions<SmtpSettings> options)
         {
-            _configuration = configuration;
+            _logger = logger;
+            _settings = options.Value;
         }
 
-        public async Task SendMailAsync(string to, string subject, string body, bool isBodyHtml = true)
+        public async Task SendAsync(string to, string subject, string htmlBody)
         {
-            await SendMailAsync(to, subject, body, isBodyHtml);
+            await SendAsync(to, subject, htmlBody, _settings.FromEmail, _settings.FromName);
         }
 
-        public async Task SendMailAsync(List<string> to, string subject, string body, bool isBodyHtml = true)
+        public async Task SendAsync(string to, string subject, string htmlBody, string from, string displayName)
         {
-            MailMessage mail = new MailMessage();
-            mail.IsBodyHtml = isBodyHtml;
-            foreach (var item in to)
-                mail.To.Add(item);
-            mail.Subject = subject;
-            mail.Body = body;
+            var message = new MailMessage
+            {
+                From = new MailAddress(from ?? _settings.FromEmail, displayName ?? _settings.FromName),
+                Subject = subject,
+                Body = htmlBody,
+                IsBodyHtml = true
+            };
 
-            mail.From = new(_configuration["Mail:Username"], "Api", Encoding.UTF8);
+            message.To.Add(to);
 
-            SmtpClient smtp = new SmtpClient();
-            smtp.Credentials = new NetworkCredential(_configuration["Mail:Username"], _configuration["Mail:Password"]);
-            smtp.Port = Convert.ToInt32(_configuration["Mail:Port"]);
-            smtp.EnableSsl = true;
-            smtp.Host = _configuration["Mail:Host"];
-            await smtp.SendMailAsync(mail);
+            using var smtp = new SmtpClient(_settings.Host, _settings.Port)
+            {
+                Credentials = new NetworkCredential(_settings.Username, _settings.Password),
+                EnableSsl = _settings.EnableSsl,
+            };
+
+            try
+            {
+                await smtp.SendMailAsync(message);
+                _logger.LogInformation($"Email sent to {to}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Email could not be sent to {to}");
+                throw;
+            }
         }
 
-        public async Task SendPasswordResetMailAsync(string to, string userId, string resetToken)
+        public async Task SendWithAttachmentAsync(string to, string subject, string htmlBody, List<(byte[] Content, string FileName, string MimeType)> attachments)
         {
-            string resetLink = $"Hi! <br> If you want reset password, yo have a link for reset password <br> <strong> <a target='blank' href='....../{userId}/{resetToken}'></a> Touch for new password </strong> <br><br><br> Montela";
+            using var message = new MailMessage
+            {
+                From = new MailAddress(_settings.FromEmail, _settings.FromName),
+                Subject = subject,
+                Body = htmlBody,
+                IsBodyHtml = true
+            };
 
-            await SendMailAsync(to, "Reset Password", resetLink);
+            message.To.Add(to);
+
+            foreach (var attachment in attachments)
+            {
+                var stream = new MemoryStream(attachment.Content);
+                message.Attachments.Add(new Attachment(stream, attachment.FileName, attachment.MimeType));
+            }
+
+            using var smtp = new SmtpClient(_settings.Host, _settings.Port)
+            {
+                Credentials = new NetworkCredential(_settings.Username, _settings.Password),
+                EnableSsl = _settings.EnableSsl
+            };
+
+            try
+            {
+                await smtp.SendMailAsync(message);
+                _logger.LogInformation($"Email with attachments sent to {to}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Failed to send email with attachments to {to}");
+                throw;
+            }
         }
     }
+
+
 }
+
+//to do - Mail servisləri ayır və MailService servisini hər birini ayrıca istifadə edə biləcək səviyyəyə gətir!
